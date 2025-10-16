4762047182bc29765f019473877a909c99b2f175
Fernando Mirasol <fhermq@gmail.com>
2025-10-16 17:17:15 -0600

    SingleNumber added method getSingleNumberWithSet

--- src/main/java/com/excercise/coding/SingleNumber.java
+++ src/main/java/com/excercise/coding/SingleNumber.java
@@ -1,12 +1,20 @@
 package com.excercise.coding;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SingleNumber {
 
     public static void main(String[] args) {
-      int[] array = {4, 1, 2, 1, 2};
-        System.out.println("Single number: " + getSingleNumber(array));   
+        int[] array = {4, 1, 2, 1, 2};
+        System.out.println("Single number: " + getSingleNumber(array));
+        System.out.println("Single number with SET: " + getSingleNumberWithSet(array));
     }
 
+    /*
+     * TC: 0(n)
+     * SC: 0(1)
+     */
     public static int getSingleNumber(int array[]) {
         int singleNumber = 0;
         for (int num : array) {
@@ -15,4 +23,20 @@ public class SingleNumber {
         return singleNumber;
     }
 
+
+    /*
+     * TC: 0(n)
+     * SC: 0(n)
+     */
+    public static int getSingleNumberWithSet(int array[]) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : array) {
+            if(set.contains(num)){
+                set.remove(num);
+            }else{
+                set.add(num);
+            }
+        }
+        return set.iterator().next();
+    }
 }
