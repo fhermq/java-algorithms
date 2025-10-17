4ae5cd940c8ceb66762e916a3abefbad3c31f9d2
Fernando Mirasol <fhermq@gmail.com>
2025-10-16 18:03:24 -0600

    Unit Test Added

--- src/main/java/com/excercise/coding/FizzBuzz.java
+++ src/main/java/com/excercise/coding/FizzBuzz.java
@@ -1,50 +1,16 @@
 package com.excercise.coding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FizzBuzz {
 
-    public static void main(String[] args) {
-        int n = 15;
-        List<String> fizzBuList;
-        String result[];
-        fizzBuList = getFizzBuzzList(n);
-        System.out.println(fizzBuList.toString());
-        fizzBuList = getFizzBuzzListTwo(n);
-        System.out.println(fizzBuList.toString());
-        result = fizzBuzzWithArray(n);
-        System.out.println(Arrays.toString(result));
-
-        //TC: 0(n)
-        //SC: 0(1)
-    }
-
-    public static List<String> getFizzBuzzList(int n) {
-        List<String> fizzBuzzList = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            String value = "";
-            if (i % 3 == 0) {
-                value += "Fizz";
-            }
-            if (i % 5 == 0) {
-                value += "Buzz";
-            }
-            if (value.isEmpty()) {
-                value += String.valueOf(i);
-            }
-            fizzBuzzList.add(value);
-        }
-        return fizzBuzzList;
-    }
-
-
     /*
+    * Best Solution
      *TC: 0(n)
      *SC: 0(1)  
      */
-    public static List<String> getFizzBuzzListTwo(int n) {
+    public List<String> getFizzBuzzList(int n) {
         List<String> fizzBuzzList = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
@@ -60,7 +26,7 @@ public class FizzBuzz {
         return fizzBuzzList;
     }
 
-    public static String[] fizzBuzzWithArray(int n) {
+    public String[] getFizzBuzzArray(int n) {
         String[] result = new String[n];
         for (int i = 1; i <= n; i++) {
             String value = "";
@@ -76,6 +42,24 @@ public class FizzBuzz {
             result[i - 1] = value;
         }
         return result;
+    }
+
+    public List<String> getFizzBuzzListOp2(int n) {
+        List<String> fizzBuzzList = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            String value = "";
+            if (i % 3 == 0) {
+                value += "Fizz";
+            }
+            if (i % 5 == 0) {
+                value += "Buzz";
+            }
+            if (value.isEmpty()) {
+                value += String.valueOf(i);
+            }
+            fizzBuzzList.add(value);
+        }
+        return fizzBuzzList;
     }
 
 }
---
+++ src/test/java/com/excercise/coding/FizzBuzzTest.java
@@ -0,0 +1,105 @@
+package com.excercise.coding;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Calculator Functionality Tests")
+class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+    String[] expected1 = {"1"};
+    String[] expected5 = {"1", "2", "Fizz", "4", "Buzz"};
+    String[] expected15 = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+
+    @BeforeEach
+    void setUp() {
+        // Initialize the object under test before each test method
+        fizzBuzz = new FizzBuzz();
+    }
+
+
+    /*
+     * Test for getFizzBuzzArray
+     */
+    @Test
+    @DisplayName("getFizzBuzzArray - Should correctly get FizzBuzz Array for 1")
+    void testgetFizzBuzzArrayWithOne() {
+        String[] actual = fizzBuzz.getFizzBuzzArray(1);
+        assertArrayEquals(expected1, actual, "Error");
+    }
+
+    @Test
+    @DisplayName("getFizzBuzzArray - Should correctly get FizzBuzz Array for 5")
+    void testgetFizzBuzzArrayWithFive() {
+        String[] actual = fizzBuzz.getFizzBuzzArray(5);
+        assertArrayEquals(expected5, actual, "Error");
+    }
+
+    @Test
+    @DisplayName("getFizzBuzzArray - Should correctly get FizzBuzz Array for 15")
+    void testgetFizzBuzzArrayWithFifteen() {
+        String[] actual = fizzBuzz.getFizzBuzzArray(15);
+        assertArrayEquals(expected15, actual, "Error");
+    }
+
+
+    /*
+     * Test for getFizzBuzzList
+     */
+    @Test
+    @DisplayName("getFizzBuzzList - Should correctly get FizzBuzz Array for 1")
+    void testgetFizzBuzzListWithOne() {
+        List<String> expected = Arrays.asList(expected1);
+        List<String> actual = fizzBuzz.getFizzBuzzList(1);
+        assertArrayEquals(expected.toArray(), actual.toArray(), "Error");
+    }
+
+    @Test
+    @DisplayName("getFizzBuzzList - Should correctly get FizzBuzz Array for 5")
+    void testgetFizzBuzzListWithFive() {
+        List<String> expected = Arrays.asList(expected5);
+        List<String> actual = fizzBuzz.getFizzBuzzList(5);
+        assertArrayEquals(expected.toArray(), actual.toArray(), "Error");
+    }
+
+    @Test
+    @DisplayName("getFizzBuzzList - Should correctly get FizzBuzz Array for 15")
+    void testgetFizzBuzzListFifteen() {
+        List<String> expected = Arrays.asList(expected15);
+        List<String> actual = fizzBuzz.getFizzBuzzList(15);
+        assertArrayEquals(expected.toArray(), actual.toArray(), "Error");
+    }
+
+    /*
+     * Test for getFizzBuzzListOp2
+     */
+    @Test
+    @DisplayName("getFizzBuzzListOp2 - Should correctly get FizzBuzz Array for 1")
+    void testgetFizzBuzzListOp2WithOne() {
+        List<String> expected = Arrays.asList(expected1);
+        List<String> actual = fizzBuzz.getFizzBuzzListOp2(1);
+        assertArrayEquals(expected.toArray(), actual.toArray(), "Error");
+    }
+
+    @Test
+    @DisplayName("getFizzBuzzListOp2 - Should correctly get FizzBuzz Array for 5")
+    void testgetFizzBuzzListOp2WithFive() {
+        List<String> expected = Arrays.asList(expected5);
+        List<String> actual = fizzBuzz.getFizzBuzzListOp2(5);
+        assertArrayEquals(expected.toArray(), actual.toArray(), "Error");
+    }
+
+    @Test
+    @DisplayName("getFizzBuzzListOp2 - Should correctly get FizzBuzz Array for 15")
+    void testgetFizzBuzzListOp2Fifteen() {
+        List<String> expected = Arrays.asList(expected15);
+        List<String> actual = fizzBuzz.getFizzBuzzListOp2(15);
+        assertArrayEquals(expected.toArray(), actual.toArray(), "Error");
+    }
+
+}
